52da3c48c8c6b495137c53dfa6d51a1750b35410
simanto008 <149402418+simanto008@users.noreply.github.com>
2025-03-10 15:06:13 +0600

    Update config.py db

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6741151244"))
 #Port
 PORT = os.environ.get("PORT", "8030")
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb://onelyon:dEMDQgeScx9pQtNX@<hostname>/?ssl=true&replicaSet=atlas-14084f-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "onepiece")
 
 #Time in seconds for message delete, put 0 to never delete
